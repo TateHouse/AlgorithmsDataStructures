359ff13abd03c25a0d2efe434594621fe012a086
Tate House <tatehouse100@gmail.com>
2023-10-11 19:26:45 -0400

    Feature: Doubly linked list insertions

--- Core/Include/DataStructures/LinkedLists/DoublyLinkedList.hpp
+++ Core/Include/DataStructures/LinkedLists/DoublyLinkedList.hpp
@@ -17,6 +17,11 @@ public:
 	BidirectionalIterator begin() const noexcept;
 	BidirectionalIterator end() const noexcept;
 
+public:
+	void insertAtHead(const ElementType& element) noexcept;
+	void insertAtTail(const ElementType& element) noexcept;
+	const bool insertAtIndex(const ElementType& element, const std::size_t index) noexcept;
+
 private:
 	std::size_t nodeCount {0};
 	DoublyLinkedListNode<ElementType>* headNode {nullptr};
@@ -41,5 +46,68 @@ DoublyLinkedList<ElementType>::BidirectionalIterator DoublyLinkedList<ElementTyp
 template<typename ElementType>
 DoublyLinkedList<ElementType>::BidirectionalIterator DoublyLinkedList<ElementType>::end() const noexcept {
 	return BidirectionalIterator(nullptr);
+}
+
+template<typename ElementType>
+void DoublyLinkedList<ElementType>::insertAtHead(const ElementType& element) noexcept {
+	auto* node {new DoublyLinkedListNode<ElementType>(element)};
+	
+	if (headNode == nullptr) {
+		headNode = node;
+		tailNode = node;
+	} else {
+		node->setNextNode(headNode);
+		headNode->setPreviousNode(node);
+		headNode = node;
+	}
+	
+	++nodeCount;
+}
+
+template<typename ElementType>
+void DoublyLinkedList<ElementType>::insertAtTail(const ElementType& element) noexcept {
+	auto* node {new DoublyLinkedListNode<ElementType>(element)};
+	
+	if (tailNode == nullptr) {
+		headNode = node;
+		tailNode = node;
+	} else {
+		node->setPreviousNode(tailNode);
+		tailNode->setNextNode(node);
+		tailNode = node;
+	}
+	
+	++nodeCount;
+}
+
+template<typename ElementType>
+const bool DoublyLinkedList<ElementType>::insertAtIndex(const ElementType& element, const std::size_t index) noexcept {
+	if (index > nodeCount) {
+		return false;
+	}
+	
+	if (index == 0 || headNode == nullptr) {
+		insertAtHead(element);
+		return true;
+	}
+	
+	if (index == nodeCount) {
+		insertAtTail(element);
+		return true;
+	}
+	
+	auto* node {new DoublyLinkedListNode<ElementType>(element)};
+	auto* previousNode {headNode};
+	for (std::size_t currentIndex {0}; currentIndex < index - 1; ++currentIndex) {
+		previousNode = previousNode->getNextNode();
+	}
+	
+	node->setNextNode(previousNode->getNextNode());
+	node->setPreviousNode(previousNode);
+	node->getNextNode()->setPreviousNode(node);
+	previousNode->setNextNode(node);
+	++nodeCount;
+	
+	return true;
 }
 }
