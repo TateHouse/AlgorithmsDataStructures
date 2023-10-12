c6dfc4b2437ebd8a01c8be8918c1d81952791c78
Tate House <tatehouse100@gmail.com>
2023-10-12 11:37:14 -0400

    Feature: Fixed-size array stack operators

--- Core/Include/DataStructures/Stacks/FixedSizeArrayStack.hpp
+++ Core/Include/DataStructures/Stacks/FixedSizeArrayStack.hpp
@@ -10,9 +10,33 @@ public:
 	FixedSizeArrayStack(const FixedSizeArrayStack<ElementType, Size>& other) noexcept = default;
 	FixedSizeArrayStack(FixedSizeArrayStack<ElementType, Size>&& other) noexcept = default;
 	~FixedSizeArrayStack() noexcept = default;
-	
+
+public:
+	FixedSizeArrayStack<ElementType, Size>& operator=(const FixedSizeArrayStack<ElementType, Size>& other) noexcept = default;
+	FixedSizeArrayStack<ElementType, Size>& operator=(FixedSizeArrayStack<ElementType, Size>&& other) noexcept = default;
+	const bool operator==(const FixedSizeArrayStack<ElementType, Size>& other) const noexcept;
+
 private:
 	std::array<ElementType, Size> array {};
 	std::size_t topIndex {0};
 };
+
+template<typename ElementType, std::size_t Size>
+const bool FixedSizeArrayStack<ElementType, Size>::operator==(const FixedSizeArrayStack<ElementType, Size>& other) const noexcept {
+	if (this == &other) {
+		return true;
+	}
+	
+	if (topIndex != other.topIndex) {
+		return false;
+	}
+	
+	for (std::size_t index {0}; index <= topIndex; ++index) {
+		if (array[index] != other.array[index]) {
+			return false;
+		}
+	}
+	
+	return true;
+}
 }
