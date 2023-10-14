0b86a4b1b33fd8d308b811ba63dc8078d3c2cb1a
Tate House <tatehouse100@gmail.com>
2023-10-14 01:32:58 -0400

    Feature: Dynamic-size array stack operators

--- Core/Include/DataStructures/Stacks/DynamicSizeArrayStack.hpp
+++ Core/Include/DataStructures/Stacks/DynamicSizeArrayStack.hpp
@@ -10,8 +10,32 @@ public:
 	DynamicSizeArrayStack(const DynamicSizeArrayStack<ElementType>& other) noexcept = default;
 	DynamicSizeArrayStack(DynamicSizeArrayStack<ElementType>&& other) noexcept = default;
 	~DynamicSizeArrayStack() noexcept = default;
-
+	
+public:
+	DynamicSizeArrayStack<ElementType>& operator=(const DynamicSizeArrayStack<ElementType>& other) noexcept = default;
+	DynamicSizeArrayStack<ElementType>& operator=(DynamicSizeArrayStack<ElementType>&& other) noexcept = default;
+	const bool operator==(const DynamicSizeArrayStack<ElementType>& other) const noexcept;
+	
 private:
 	std::vector<ElementType> vector {};
 };
+
+template<typename ElementType>
+const bool DynamicSizeArrayStack<ElementType>::operator==(const DynamicSizeArrayStack<ElementType>& other) const noexcept {
+	if (this == &other) {
+		return true;
+	}
+	
+	if (vector.size() != other.vector.size()) {
+		return false;
+	}
+	
+	for (std::size_t index {0}; index < vector.size(); ++index) {
+		if (vector[index] != other.vector[index]) {
+			return false;
+		}
+	}
+	
+	return true;
+}
 }
