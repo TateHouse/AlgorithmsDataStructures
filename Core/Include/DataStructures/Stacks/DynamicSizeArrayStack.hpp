b9dc209fdb731b0a3846f6f174f3f4f452a75e63
Tate House <tatehouse100@gmail.com>
2023-10-14 01:30:56 -0400

    Feature: Dynamic-size array stack constructors

---
+++ Core/Include/DataStructures/Stacks/DynamicSizeArrayStack.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <vector>
+
+namespace Core::DataStructures::Stacks {
+template<typename ElementType>
+class DynamicSizeArrayStack final {
+public:
+	DynamicSizeArrayStack() noexcept = default;
+	DynamicSizeArrayStack(const DynamicSizeArrayStack<ElementType>& other) noexcept = default;
+	DynamicSizeArrayStack(DynamicSizeArrayStack<ElementType>&& other) noexcept = default;
+
+private:
+	std::vector<ElementType> vector {};
+};
+}
