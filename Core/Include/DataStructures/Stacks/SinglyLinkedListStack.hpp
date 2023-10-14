67e27b90a8b374aae07531befc42ee532181380c
Tate House <tatehouse100@gmail.com>
2023-10-14 02:05:03 -0400

    Feature: Singly linked list stack push

--- Core/Include/DataStructures/Stacks/SinglyLinkedListStack.hpp
+++ Core/Include/DataStructures/Stacks/SinglyLinkedListStack.hpp
@@ -15,6 +15,10 @@ public:
 	SinglyLinkedListStack<ElementType>& operator=(const SinglyLinkedListStack<ElementType>& other) noexcept = default;
 	SinglyLinkedListStack<ElementType>& operator=(SinglyLinkedListStack<ElementType>&& other) noexcept = default;
 	const bool operator==(const SinglyLinkedListStack<ElementType>& other) const noexcept;
+	
+public:
+	void push(const ElementType& element) noexcept;
+	void push(ElementType&& element) noexcept;
 
 private:
 	LinkedLists::SinglyLinkedList<ElementType> singlyLinkedList {};
@@ -27,5 +31,15 @@ const bool SinglyLinkedListStack<ElementType>::operator==(const SinglyLinkedList
 	}
 	
 	return singlyLinkedList == other.singlyLinkedList;
+}
+
+template<typename ElementType>
+void SinglyLinkedListStack<ElementType>::push(const ElementType& element) noexcept {
+	singlyLinkedList.insertAtFront(element);
+}
+
+template<typename ElementType>
+void SinglyLinkedListStack<ElementType>::push(ElementType&& element) noexcept {
+	singlyLinkedList.insertAtFront(std::move(element));
 }
 }
