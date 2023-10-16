0b46207774365ee88f315993202e52e2b1b9f557
Tate House <tatehouse100@gmail.com>
2023-10-16 01:04:18 -0400

    Feature: Fixed-size circular queue operators

--- Core/Include/DataStructures/Queues/FixedSizeCircularArrayQueue.hpp
+++ Core/Include/DataStructures/Queues/FixedSizeCircularArrayQueue.hpp
@@ -11,10 +11,34 @@ public:
 	FixedSizeCircularArrayQueue(FixedSizeCircularArrayQueue<ElementType, Size>&& other) noexcept = default;
 	~FixedSizeCircularArrayQueue() noexcept = default;
 
+public:
+	FixedSizeCircularArrayQueue<ElementType, Size>& operator=(const FixedSizeCircularArrayQueue<ElementType, Size>& other) noexcept = default;
+	FixedSizeCircularArrayQueue<ElementType, Size>& operator=(FixedSizeCircularArrayQueue<ElementType, Size>&& other) noexcept = default;
+	const bool operator==(const FixedSizeCircularArrayQueue<ElementType, Size>& other) const noexcept;
+
 private:
 	std::array<ElementType, Size> array {};
 	std::size_t elementCount {0};
 	std::size_t frontIndex {0};
 	std::size_t backIndex {Size - 1};
 };
+
+template<typename ElementType, std::size_t Size>
+const bool FixedSizeCircularArrayQueue<ElementType, Size>::operator==(const FixedSizeCircularArrayQueue<ElementType, Size>& other) const noexcept {
+	if (this == &other) {
+		return true;
+	}
+	
+	if (elementCount != other.elementCount || other.frontIndex != frontIndex || other.backIndex != backIndex) {
+		return false;
+	}
+	
+	for (std::size_t index {0}; index < elementCount; ++index) {
+		if (array[(frontIndex + index) % Size] != other.array[(other.frontIndex + index) % Size]) {
+			return false;
+		}
+	}
+	
+	return true;
+}
 }
