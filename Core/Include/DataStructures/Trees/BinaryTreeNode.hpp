1318d83279d7e973c03d11a2d4b939e38f5fb3d9
Tate House <tatehouse100@gmail.com>
2023-10-17 15:18:01 -0400

    Feature: Binary tree node operators

--- Core/Include/DataStructures/Trees/BinaryTreeNode.hpp
+++ Core/Include/DataStructures/Trees/BinaryTreeNode.hpp
@@ -12,6 +12,15 @@ public:
 	BinaryTreeNode(BinaryTreeNode<ElementType>&& other) noexcept = default;
 	~BinaryTreeNode() noexcept = default;
 
+public:
+	BinaryTreeNode<ElementType>& operator=(const BinaryTreeNode<ElementType>& other) noexcept = default;
+	BinaryTreeNode<ElementType>& operator=(BinaryTreeNode<ElementType>&& other) noexcept = default;
+	const bool operator==(const BinaryTreeNode<ElementType>& other) const noexcept;
+
+private:
+	static const bool areChildrenEqual(const BinaryTreeNode<ElementType>* leftChild,
+	                                   const BinaryTreeNode<ElementType>* rightChild) noexcept;
+
 private:
 	ElementType element;
 	BinaryTreeNode<ElementType>* leftChild {nullptr};
@@ -19,12 +28,41 @@ private:
 };
 
 template<typename ElementType>
-BinaryTreeNode<ElementType>::BinaryTreeNode(const ElementType& element) noexcept : element{element} {
+BinaryTreeNode<ElementType>::BinaryTreeNode(const ElementType& element) noexcept : element {element} {
 
 }
 
 template<typename ElementType>
-BinaryTreeNode<ElementType>::BinaryTreeNode(ElementType&& element) noexcept : element{std::move(element)} {
+BinaryTreeNode<ElementType>::BinaryTreeNode(ElementType&& element) noexcept : element {std::move(element)} {
 
+}
+
+template<typename ElementType>
+const bool BinaryTreeNode<ElementType>::operator==(const BinaryTreeNode<ElementType>& other) const noexcept {
+	if (element != other.element) {
+		return false;
+	}
+	
+	return BinaryTreeNode<ElementType>::areChildrenEqual(leftChild, other.leftChild) &&
+	       BinaryTreeNode<ElementType>::areChildrenEqual(rightChild, other.rightChild);
+}
+
+template<typename ElementType>
+const bool BinaryTreeNode<ElementType>::areChildrenEqual(const BinaryTreeNode<ElementType>* leftChild,
+                                                                const BinaryTreeNode<ElementType>* rightChild) noexcept {
+	if (leftChild == nullptr && rightChild == nullptr) {
+		return true;
+	}
+	
+	if (leftChild == nullptr || rightChild == nullptr) {
+		return false;
+	}
+	
+	if (leftChild->element != rightChild->element) {
+		return false;
+	}
+	
+	return areChildrenEqual(leftChild->leftChild, rightChild->leftChild) &&
+	       areChildrenEqual(leftChild->rightChild, rightChild->rightChild);
 }
 }
