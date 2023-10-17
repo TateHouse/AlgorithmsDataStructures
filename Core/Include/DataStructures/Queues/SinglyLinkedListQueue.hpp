43166953f6ddf710f5b96a21f459f51383d83d14
Tate House <tatehouse100@gmail.com>
2023-10-17 14:16:42 -0400

    Feature: Singly linked list queue constructors

---
+++ Core/Include/DataStructures/Queues/SinglyLinkedListQueue.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "DataStructures/LinkedLists/SinglyLinkedList.hpp"
+
+namespace Core::DataStructures::Queues {
+template<typename ElementType>
+class SinglyLinkedListQueue final {
+public:
+	SinglyLinkedListQueue() noexcept = default;
+	SinglyLinkedListQueue(const SinglyLinkedListQueue<ElementType>& other) = default;
+	SinglyLinkedListQueue(SinglyLinkedListQueue<ElementType>&& other) noexcept = default;
+
+private:
+	LinkedLists::SinglyLinkedList<ElementType> singlyLinkedList {};
+};
+}
