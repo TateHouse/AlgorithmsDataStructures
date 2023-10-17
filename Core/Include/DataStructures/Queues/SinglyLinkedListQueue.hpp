1eeeb3a503094bf81f49346b301245fad95b6351
Tate House <tatehouse100@gmail.com>
2023-10-17 14:18:09 -0400

    Feature: Singly linked list queue operators

--- Core/Include/DataStructures/Queues/SinglyLinkedListQueue.hpp
+++ Core/Include/DataStructures/Queues/SinglyLinkedListQueue.hpp
@@ -11,7 +11,17 @@ public:
 	SinglyLinkedListQueue(SinglyLinkedListQueue<ElementType>&& other) noexcept = default;
 	~SinglyLinkedListQueue() noexcept = default;
 
+public:
+	SinglyLinkedListQueue<ElementType>& operator=(const SinglyLinkedListQueue<ElementType>& other) = default;
+	SinglyLinkedListQueue<ElementType>& operator=(SinglyLinkedListQueue<ElementType>&& other) noexcept = default;
+	const bool operator==(const SinglyLinkedListQueue<ElementType>& other) const noexcept;
+
 private:
 	LinkedLists::SinglyLinkedList<ElementType> singlyLinkedList {};
 };
+
+template<typename ElementType>
+const bool SinglyLinkedListQueue<ElementType>::operator==(const SinglyLinkedListQueue<ElementType>& other) const noexcept {
+	return singlyLinkedList == other.singlyLinkedList;
+}
 }
