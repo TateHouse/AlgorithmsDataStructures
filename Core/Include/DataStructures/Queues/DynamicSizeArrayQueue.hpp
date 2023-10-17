e28d0e665005a7a85cd025a8f53fa1f7bce82b5d
Tate House <tatehouse100@gmail.com>
2023-10-17 09:52:50 -0400

    Feature: Dynamic-size array queue operators

--- Core/Include/DataStructures/Queues/DynamicSizeArrayQueue.hpp
+++ Core/Include/DataStructures/Queues/DynamicSizeArrayQueue.hpp
@@ -10,8 +10,32 @@ public:
 	DynamicSizeArrayQueue(const DynamicSizeArrayQueue<ElementType>& other) noexcept = default;
 	DynamicSizeArrayQueue(DynamicSizeArrayQueue<ElementType>&& other) noexcept = default;
 	~DynamicSizeArrayQueue() noexcept = default;
+	
+public:
+	DynamicSizeArrayQueue<ElementType>& operator=(const DynamicSizeArrayQueue<ElementType>& other) noexcept = default;
+	DynamicSizeArrayQueue<ElementType>& operator=(DynamicSizeArrayQueue<ElementType>&& other) noexcept = default;
+	const bool operator==(const DynamicSizeArrayQueue<ElementType>& other) const noexcept;
 
 private:
 	std::vector<ElementType> vector {};
 };
+
+template<typename ElementType>
+const bool DynamicSizeArrayQueue<ElementType>::operator==(const DynamicSizeArrayQueue<ElementType>& other) const noexcept {
+	if (this == &other) {
+		return true;
+	}
+	
+	if (vector.size() != other.vector.size()) {
+		return false;
+	}
+	
+	for (std::size_t index {0}; index < vector.size(); ++index) {
+		if (vector[index] != other.vector[index]) {
+			return false;
+		}
+	}
+	
+	return true;
+}
 }
