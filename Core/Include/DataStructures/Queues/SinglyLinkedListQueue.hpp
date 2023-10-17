d9281312e66ebf990af2cafbdac0268e5b954666
Tate House <tatehouse100@gmail.com>
2023-10-17 14:16:53 -0400

    Feature: Singly linked list queue destructor

--- Core/Include/DataStructures/Queues/SinglyLinkedListQueue.hpp
+++ Core/Include/DataStructures/Queues/SinglyLinkedListQueue.hpp
@@ -9,6 +9,7 @@ public:
 	SinglyLinkedListQueue() noexcept = default;
 	SinglyLinkedListQueue(const SinglyLinkedListQueue<ElementType>& other) = default;
 	SinglyLinkedListQueue(SinglyLinkedListQueue<ElementType>&& other) noexcept = default;
+	~SinglyLinkedListQueue() noexcept = default;
 
 private:
 	LinkedLists::SinglyLinkedList<ElementType> singlyLinkedList {};
