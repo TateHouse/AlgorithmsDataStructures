2f9c460e7eccb49a248c66f0d0dd69b45e9da91f
Tate House <tatehouse100@gmail.com>
2023-10-17 14:58:33 -0400

    Feature: Binary tree node constructors

---
+++ Core/Include/DataStructures/Trees/BinaryTreeNode.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <utility>
+
+namespace Core::DataStructures::Trees {
+template<typename ElementType>
+class BinaryTreeNode final {
+public:
+	explicit BinaryTreeNode(const ElementType& element) noexcept;
+	explicit BinaryTreeNode(ElementType&& element) noexcept;
+	BinaryTreeNode(const BinaryTreeNode<ElementType>& other) noexcept = default;
+	BinaryTreeNode(BinaryTreeNode<ElementType>&& other) noexcept = default;
+
+private:
+	ElementType element;
+	BinaryTreeNode<ElementType>* leftChild {nullptr};
+	BinaryTreeNode<ElementType>* rightChild {nullptr};
+};
+
+template<typename ElementType>
+BinaryTreeNode<ElementType>::BinaryTreeNode(const ElementType& element) noexcept : element{element} {
+
+}
+
+template<typename ElementType>
+BinaryTreeNode<ElementType>::BinaryTreeNode(ElementType&& element) noexcept : element{std::move(element)} {
+
+}
+}
