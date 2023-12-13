5dc646bbb7cd926ded0fcc00d15ff9e86ce9bb01
Tate House <tatehouse100@gmail.com>
2023-12-13 14:54:04 -0500

    Docs: Add method documentation

--- Core/Include/DataStructures/Trees/BinaryTreeNode.hpp
+++ Core/Include/DataStructures/Trees/BinaryTreeNode.hpp
@@ -3,31 +3,124 @@
 #include <utility>
 
 namespace Core::DataStructures::Trees {
+/**
+ * @brief A node for binary trees.
+ * @class BinaryTreeNode
+ * @tparam ElementType The type of the elements stored in the binary tree.
+ */
 template<typename ElementType>
 class BinaryTreeNode final {
 public:
+	/**
+	 * @brief Instantiates a new binary tree node with the given element using copy semantics.
+	 * @param element The element to store in the node.
+	 */
 	explicit BinaryTreeNode(const ElementType& element) noexcept;
+	
+	/**
+	 * @brief Instantiates a new binary tree node with the given element using move semantics.
+	 * @param element The element to store in the node.
+	 */
 	explicit BinaryTreeNode(ElementType&& element) noexcept;
+	
+	/**
+	 * @brief Instantiates a new binary tree node by copying the given node.
+	 * @param other The node to copy.
+	 */
 	BinaryTreeNode(const BinaryTreeNode<ElementType>& other) noexcept = default;
+	
+	/**
+	 * @brief Instantiates a new binary tree node by moving the given node.
+	 * @param other The node to move.
+	 */
 	BinaryTreeNode(BinaryTreeNode<ElementType>&& other) noexcept = default;
+	
+	/**
+	 * @brief Destroys the node.
+	 */
 	~BinaryTreeNode() noexcept = default;
 
 public:
+	/**
+	 * @brief Assigns the given node to this node using copy semantics.
+	 * @param other The node to copy.
+	 * @return A reference to this node.
+	 */
 	BinaryTreeNode<ElementType>& operator=(const BinaryTreeNode<ElementType>& other) noexcept = default;
+	
+	/**
+	 * @brief Assigns the given node to this node using move semantics.
+	 * @param other The node to move.
+	 * @return A reference to this node.
+	 */
 	BinaryTreeNode<ElementType>& operator=(BinaryTreeNode<ElementType>&& other) noexcept = default;
+	
+	/**
+	 * @brief Checks if this node is equal to the given node.
+	 * @details Two nodes are considered equal if their elements and children are equal.
+	 * @param other The node to compare with.
+	 * @return True if the two nodes are equal, false otherwise.
+	 */
 	const bool operator==(const BinaryTreeNode<ElementType>& other) const noexcept;
 
 public:
+	/**
+	 * @brief Gets a reference to the element stored in the node.
+	 * @return A reference to the element stored in the node.
+	 */
 	ElementType& getElement() noexcept;
+	
+	/**
+	 * @brief Gets a const reference to the element stored in the node.
+	 * @return A const reference to the element stored in the node.
+	 */
 	const ElementType& getElement() const noexcept;
+	
+	/**
+	 * @brief Gets a pointer to the left child of the node.
+	 * @return A pointer to the left child of the node.
+	 */
 	BinaryTreeNode<ElementType>* getLeftChild() noexcept;
+	
+	/**
+	 * @brief Gets a const pointer to the left child of the node.
+	 * @return A const pointer to the left child of the node.
+	 */
 	const BinaryTreeNode<ElementType>* getLeftChild() const noexcept;
+	
+	/**
+	 * @brief Sets the left child of the node.
+	 * @param leftChild A pointer to the left child of the node.
+	 */
 	void setLeftChild(BinaryTreeNode<ElementType>* leftChild) noexcept;
+	
+	/**
+	 * @brief Gets a pointer to the right child of the node.
+	 * @return A pointer to the right child of the node.
+	 */
 	BinaryTreeNode<ElementType>* getRightChild() noexcept;
+	
+	/**
+	 * @brief Gets a const pointer to the right child of the node.
+	 * @return A const pointer to the right child of the node.
+	 */
 	const BinaryTreeNode<ElementType>* getRightChild() const noexcept;
+	
+	/**
+	 * @brief Sets the right child of the node.
+	 * @param rightChild A pointer to the right child of the node.
+	 */
 	void setRightChild(BinaryTreeNode<ElementType>* rightChild) noexcept;
 
 private:
+	/**
+	 * @brief Checks if the given children are equal.
+	 * @details Two children are considered equal if they are both nullptr or if they are both not nullptr and their
+	 * elements and children are equal.
+	 * @param leftChild The left child of the node.
+	 * @param rightChild The right child of the node.
+	 * @return True if the children are equal, false otherwise.
+	 */
 	static const bool areChildrenEqual(const BinaryTreeNode<ElementType>* leftChild,
 	                                   const BinaryTreeNode<ElementType>* rightChild) noexcept;
 
